94efed47f85ec9dbe035bb10dc098e6f660deb4c
Qiwei Lin <qiweilin@uchicago.edu>
2021-11-01 09:58:46 -0500

    Add scripts for Covid

---
+++ Data_Wrangling/Covid_Scraping.R
@@ -0,0 +1,38 @@
+library(tidyr)
+library(dplyr)
+library(RSocrata)
+
+#setwd('E:/Spatial DS RA/OpenAirQ-dashboard/Data_Wrangling/')
+
+### Scrape COVID data from Chicago data Portal
+covid_chicago <- read.socrata("https://data.cityofchicago.org/resource/yhhz-zm2v.json")
+
+### Calculate 7-day average total case
+### Can get hospitality rate as well
+covid_raw <- covid_chicago %>%
+  rename(Date = week_start) %>%
+  filter(Date >= '2020-12-01') %>%
+  arrange(desc(Date)) %>%
+  dplyr::select(zip_code, Date, case_rate_weekly) %>%
+  pivot_wider(names_from = 'Date', values_from = 'case_rate_weekly') %>%
+  rename(zip = zip_code)
+
+zipcode <- covid_raw$zip
+
+covid <- dplyr::select(covid_raw, -zip)
+colnames(covid) <- gsub("-", "", colnames(covid))
+colnames(covid) <- paste0('COVID_Week_', colnames(covid))
+covid <- cbind(zipcode, covid)
+
+write.csv(covid, file = "../Data/Covid/CovidWeekly.csv")
+
+# calculate mean covid cases across all localities
+covid_means <- covid %>% 
+  pivot_longer(col = -zipcode, names_to = 'time', 
+               values_to = "count") %>% 
+  group_by(time) %>% 
+  mutate(count = as.numeric(count)) %>%
+  summarize(mean_covid = mean(count, na.rm = T)) %>% 
+  arrange(desc(time))
+
+write.csv(covid_means, file = "../Data/Covid/covid_means.csv")
